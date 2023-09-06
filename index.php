50c8c6fcc6bcca2729daa7b20d822b2c0448bf0c
gabriel.santos <gabrieldossantossilva2108@gmail.com>
2023-09-05 21:56:02 -0300

    FEAT: classe Aluno

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
 require_once "vendor/autoload.php";
+use Desafio\Academico\Domain\Aluno;
+
+$aluno = new Aluno();
+$aluno->setNome('Gabriel');
+$aluno->setIdade(20);
+$aluno->setCpf('80021202990');
---
+++ src/Academico/Domain/Aluno.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Desafio\Academico\Domain;
+
+use Exception;
+
+class Aluno
+{
+    private $nome;
+    private $idade;
+    private $cpf;
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void
+    {
+        if ($this->validaNome($nome)) {
+            $this->nome = $nome;
+            return;
+        }
+        throw new Exception('Nome inválido');
+    }
+
+    public function getIdade(): int
+    {
+        return $this->idade;
+    }
+
+    public function setIdade(int $idade): void
+    {
+        if ($this->validaIdade($idade)) {
+            $this->idade = $idade;
+            return;
+        }
+        throw new Exception('Idade inválida');
+    }
+
+    public function getCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): void
+    {
+        if ($this->validaCpf($cpf)) {
+            $this->cpf = $cpf;
+            return;
+        }
+        throw new Exception('Cpf inválido');
+    }
+
+    private function validaNome(string $nome): bool
+    {
+        return strlen($nome) >= 5;
+    }
+
+    private function validaIdade(int $idade): bool
+    {
+        return $idade > 18;
+    }
+
+    private function validaCpf(string $cpf): bool
+    {
+        return strlen($cpf) == 11;
+    }
+}
